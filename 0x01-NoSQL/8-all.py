010f8f2f001bc5c7252c0526e8a90f1655276c95
Lebogang <lebogangodeveloper@gmail.com>
2024-01-22 01:48:16 -0800

    Script that list all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module that list all documents in Python
+"""
+
+from pymongo.collection import Collection
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        List of documents in the collection
+    """
+    return list(mongo_collection.find())
