ee553e632ed62d10bf928a78de59e3c047d1bc76
Lebogang <lebogangodeveloper@gmail.com>
2024-01-23 00:57:02 -0800

    Script that returns all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-""" Module for interacting with PyMongo """
+"""
+Module for interacting with PyMongo
+"""
 
-from pymongo.collection import Collection
-from typing import List
 
-
-def top_students(mongo_collection: Collection) -> List[dict]:
+def top_students(mongo_collection):
     """
-    Returns all students from the MongoDB collection.
-
-    Args:
-        mongo_collection (pymongo.collection.Collection)
-
-    Returns:
-        List[dict]: A list of all student documents in the collection.
+    Returns all students from the MongoDB collection
     """
     return list(mongo_collection.find())
