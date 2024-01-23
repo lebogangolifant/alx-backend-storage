b3489b3d60bcaa178f52a3ce0ab180967ae1f0c2
Lebogang <lebogangodeveloper@gmail.com>
2024-01-23 00:26:41 -0800

    Script that provides stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -24,3 +24,14 @@ def log_stats(mongo_collection):
     status_check_count = mongo_collection.count_documents
     ({"method": "GET", "path": "/status"})
     print("{} status check".format(status_check_count))
+
+
+if __name__ == "__main__":
+    """
+    Database: logs
+    Collection: nginx
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    log_stats(logs_collection)
