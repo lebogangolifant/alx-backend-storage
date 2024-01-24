7d3256aead4bee748b5c6a525cb1d55c20221380
Lebogang <lebogangodeveloper@gmail.com>
2024-01-24 09:11:47 +0000

    Add get method with optional data conversion
    - Implemented get method that supports optional data conversion using a callable.
    - Added get_str method to automatically retrieve and decode strings from Redis.
    - Added get_int method to automatically retrieve integers from Redis.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Module Description: Implementation of the Cache class using Redis.
+Module that Cache class using Redis.
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,13 +25,31 @@ class Cache:
         """
         Method stores the input data
         in Redis using a random key and returns the key.
-
-        Args:
-        - data (Union[str, bytes, int, float]): The data to be stored.
-
-        Returns:
-        - str: The randomly generated key used for storage.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> Union[str, bytes, int, float]:
+        """
+        Method retrieves data from Redis
+        using a key and applies the optional conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return data
+
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Method retrieves a string from Redis using a key.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Method retrieves an integer from Redis using a key.
+        """
+        return self.get(key, fn=int)
