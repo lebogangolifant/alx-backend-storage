5dc0e79eaf2eddcb91c09de1e69461589c684072
Lebogang <lebogangodeveloper@gmail.com>
2024-01-24 10:51:24 +0000

    Implement call_history decorator
    - Added call_history decorator to store the history of inputs and outputs for a function in Redis.
    - Decorated the store method in the Cache class with call_history to capture input and output history.
    - Demonstrated call_history functionality in 2-main.py.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,6 +26,28 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs
+    and outputs for a function in Redis.
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper method
+        """
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+
+    return wrapper
+
+
 class Cache:
     """
     Represents a cache using Redis.
